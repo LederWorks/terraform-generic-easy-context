400f776e08d61604dc33d9d0828346f3982feff0
Ledermayer <18106157+Ledermayer@users.noreply.github.com>
2024-04-17 00:51:06 +0200

    recursive

--- examples/01-choco/test.tf
+++ examples/01-choco/test.tf
@@ -23,3 +23,6 @@ module "context_choco_azure_dev" {
 output "context_choco_azure_dev" {
   value = module.context_choco_azure_dev.context
 }
+output "all_choco" {
+  value = module.context_choco_azure_dev.all_choco
+}
