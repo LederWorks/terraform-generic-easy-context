7c27ba3102da9ec7e9d76cc50ab34e10ee60b5dc
Ledermayer <18106157+Ledermayer@users.noreply.github.com>
2024-04-17 00:32:48 +0200

    01 outputs

---
+++ modules/choco/outputs.tf
@@ -0,0 +1,21 @@
+#context
+output "context" {
+  value = local.context
+}
+
+#Direct Data Access
+output "azure" {
+  value = local.azure
+}
+output "aws" {
+  value = local.aws
+}
+output "gcp" {
+  value = local.gcp
+}
+output "oci" {
+  value = local.oci
+}
+output "onprem" {
+  value = local.onprem
+}
