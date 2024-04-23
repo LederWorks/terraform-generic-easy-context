e3c483df3a67633e76657395034ff7cecdcbf4d1
Ledermayer <18106157+Ledermayer@users.noreply.github.com>
2024-04-23 05:23:22 +0200

    fixing gcp test region

--- examples/01-choco/gcp_tst.tf
+++ examples/01-choco/gcp_tst.tf
@@ -4,7 +4,7 @@ module "context_choco_gcp_tst" {
   #Common
   cloud       = "gcp"
   environment = "tst"
-  region      = "westeurope"
+  region      = "us-east1"
 
   #General
   department = "marketing"
